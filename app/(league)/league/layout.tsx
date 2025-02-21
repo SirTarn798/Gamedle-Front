b598aead41ddfcd50cbd787cc2ea054f276995eb
Tarn Jirayu <jaoaurai@gmail.com>
2025-02-21 16:23:01 +0700

    update league classic

--- app/(league)/league/layout.tsx
+++ app/(league)/league/layout.tsx
@@ -16,15 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${jersey.className} overflow-x-hidden bg-mainTheme`}>
-        <div className="relative flex flex-col items-center w-full h-dvh justify-between bg-[url(/ArcadeBG.jpg)] bg-cover bg-center">
-          <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-          <div className="relative z-10 w-full flex flex-col items-center">
-
-          <NavBarLogin/>
-          {children}
-          </div>
-        </div>
+      <body
+        className={`${jersey.className} overflow-x-hidden bg-mainTheme bg-[url(/ArcadeBG.png)] bg-cover bg-center bg-fixed`}
+      >
+        <NavBarLogin />
+        <div className="w-full flex flex-col items-center">{children}</div>
       </body>
     </html>
   );
---
+++ app/components/LeagueClassicItem.tsx
@@ -0,0 +1,50 @@
+import { LeagueClassicInput } from "@/lib/type";
+
+export default LeagueClassicItem;
+
+const getStatusColor = (status: boolean) => {
+  return status ? "bg-green-400" : "bg-orange-400";
+};
+
+function LeagueClassicItem(props: LeagueClassicInput) {
+  return (
+    <div className="w-full grid grid-cols-6 items-center gap-4">
+      <div className="flex justify-center">
+        <div className="w-12 h-12 overflow-hidden rounded">
+          <img
+            src={props.image}
+            alt="Champion"
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </div>
+      <div
+        className={`p-2 text-center rounded ${getStatusColor(props.role.status)}`}
+      >
+        {props.role.role}
+      </div>
+      <div
+        className={`p-2 text-center rounded ${getStatusColor(props.type.status)}`}
+      >
+        {props.type.type}
+      </div>
+      <div
+        className={`p-2 text-center rounded ${getStatusColor(props.range.status)}`}
+      >
+        {props.range.range}
+      </div>
+      <div
+        className={`p-2 text-center rounded ${getStatusColor(
+          props.resource.status
+        )}`}
+      >
+        {props.resource.resource}
+      </div>
+      <div
+        className={`p-2 text-center rounded ${getStatusColor(props.gender.status)}`}
+      >
+        {props.gender.gender}
+      </div>
+    </div>
+  );
+}
