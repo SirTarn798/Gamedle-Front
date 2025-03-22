924c0bc82c7e14169f7cccb685e84b4e5e136019
peth02 <oopeth139oo@gmail.com>
2025-03-22 17:16:26 +0700

    edit error message

--- app/(auth)/login/actions.tsx
+++ app/(auth)/login/actions.tsx
@@ -1,67 +1,47 @@
 "use server";
 
-import { z } from "zod";
 import { createSession, deleteSession } from "@/lib/session";
 import { redirect } from "next/navigation";
 
-const testUser = {
-  id: "1",
-  name: "steve",
-  role: "user",
-  email: "admin@gmail.com",
-  password: "12345678",
-};
-const testUserStaff = {
-  id: "1",
-  name: "steve",
-  role: "staff",
-  email: "admin@gmail.com",
-  password: "12345678",
-};
-
-const loginSchema = z.object({
-  email: z.string().email({ message: "Invalid email address" }).trim(),
-  password: z
-    .string()
-    .min(8, { message: "Password must be at least 8 characters" })
-    .trim(),
-  previousUrl: z.string().optional(), // Add previousUrl to schema
-});
-
 export async function login(prevState: any, formData: FormData) {
   console.log(formData)
-  // console.log(formData.get('email')); // Correct way to get email
-  // console.log(formData.get('password')); // Correct way to get password
   const userEmail = formData.get('email');
   const userPassword = formData.get('password');
 
-  const response = await fetch('http://localhost/api/login', {
-    method: 'POST',
-    headers: {
-      Accept: 'application.json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ email: userEmail, password: userPassword }),
-  });
-  console.log(response);
-  const data = await response.json();
-  console.log("Login successful:", data);
-
-  const message = data.message;
-  const token = data.token;
-  const user = data.user;
-
-  if ( response.status == 200) {
-    console.log("200 OK p ja")
-  } else {
+  try {
+    const response = await fetch('http://localhost/api/login', {
+      method: 'POST',
+      headers: {
+        Accept: 'application.json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email: userEmail, password: userPassword }),
+    });
+    console.log(response);
+    const data = await response.json();
+    console.log("Login successful:", data);
+  
+    const message = data.message;
+    const token = data.token;
+    const user = data.user;
+  
+    if ( response.status != 200) {
+      return {
+        errors: {
+          message: message,
+        },
+      };
+    }
+  
+    await createSession(token, user);
+    redirect("/");
+  } catch (error) {
     return {
-      error: message,
+      errors: {
+        message: "Cannot connect to server",
+      },
     };
   }
-
-  await createSession(token, user);
-  redirect("/");
-
 
 
   // const result = loginSchema.safeParse(Object.fromEntries(formData));
