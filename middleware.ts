155b82d02ac8323adacee37ed96f979284f4816b
TarnJirayu <jaoaurai@gmail.com>
2025-03-23 22:52:23 +0700

    upload images to json file *not uploading to R2 yet*

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { decrypt } from "@/lib/session";
 import { redirect } from "next/dist/server/api-utils";
 
-const protectedRoutes = ["/upload"];
+const protectedRoutes = [""];
 const publicRoutes = ["/login"];
 
 export default async function middleware(req: NextRequest) {
