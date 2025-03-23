b5bbd5a96c2cd64bfc1f3cbd8739df42df9144e2
peth02 <oopeth139oo@gmail.com>
2025-03-23 17:42:25 +0700

    style pages

--- app/(league)/league/classic/page.tsx
+++ app/(league)/league/classic/page.tsx
@@ -5,11 +5,10 @@ import { championsData } from "@/lib/exampleData";
 
 function LeagueClassic() {
   return (
-    <div className="flex flex-col items-center gap-8 mt-12 w-full max-w-5xl">
-      <h1 className="text-white text-5xl tracking-wider pixelBorder bg-mainTheme">
+    <div className="flex flex-col items-center gap-8 mt-12 mb-24 w-full max-w-5xl">
+      <h1 className="text-white text-5xl tracking-wider pixelBorder bg-mainTheme cursor-default">
         Guess The Champion
       </h1>
-      
       <form action="" className="w-full relative mb-8">
         <input
           type="text"
@@ -23,8 +22,8 @@ function LeagueClassic() {
         </button>
       </form>
 
-      <div className="w-full grid grid-cols-6 gap-4">
-        <div className="text-xl p-2 border-b-2 text-center">Image</div>
+      <div className="w-full grid grid-cols-6 gap-4 text-white cursor-default">
+        <div className="text-xl p-2 border-b-2 text-center">Champion</div>
         <div className="text-xl p-2 border-b-2 text-center">Role</div>
         <div className="text-xl p-2 border-b-2 text-center">Type</div>
         <div className="text-xl p-2 border-b-2 text-center">Range</div>
--- app/(pokemon)/pokemon/classic/page.tsx
+++ app/(pokemon)/pokemon/classic/page.tsx
@@ -3,33 +3,35 @@
 import LeagueClassicItem from "@/app/components/LeagueClassicItem";
 import { championsData } from "@/lib/exampleData";
 
-function LeagueClassic() {
+export default function PokemonClassic() {
   return (
-    <div className="flex flex-col items-center gap-8 mt-12 w-full max-w-5xl">
-      <h1 className="text-white text-5xl tracking-wider pixelBorder bg-mainTheme">
-        Guess The Champion
+    <div className="flex flex-col items-center gap-8 mt-12 mb-24 w-full max-w-5xl">
+      <h1 className="gameBorder1 text-5xl tracking-wider cursor-default text:center">
+        Who's that Pokémon?
       </h1>
-      
-      <form action="" className="w-full relative mb-8">
-        <input
-          type="text"
-          className="w-full p-3 bg-mainTheme border-4 border-white text-2xl text-white"
-        />
-        <button
-          type="submit"
-          className="absolute right-3 top-1/2 -translate-y-1/2"
-        >
-          <img src="/arrowheads.png" alt="Submit" width={65} />
-        </button>
+      <form action="" className="relative mb-8">
+        <div className=" gameBorder2 flex flex-row flex-nowrap gap-[30px] px-[30px] py-[15px]">
+          <input
+            type="text"
+            className="w-75% focus:outline-none focus:ring-0 border-none bg-transparent text-2xl text-white"
+          />
+          <button
+            type="submit"
+            className=""
+          >
+            <img className="animate" src="/enter.png" alt="Submit" width={65} />
+          </button>
+        </div>
       </form>
 
-      <div className="w-full grid grid-cols-6 gap-4">
-        <div className="text-xl p-2 border-b-2 text-center">Image</div>
-        <div className="text-xl p-2 border-b-2 text-center">Role</div>
-        <div className="text-xl p-2 border-b-2 text-center">Type</div>
-        <div className="text-xl p-2 border-b-2 text-center">Range</div>
-        <div className="text-xl p-2 border-b-2 text-center">Resource</div>
-        <div className="text-xl p-2 border-b-2 text-center">Gender</div>
+      <div className="w-full grid grid-cols-7 gap-4 text-white font-bold text-shadow-xl cursor-default">
+        <div className="text-xl p-2 border-b-2 text-center tooltip">Pokémon<span className="tooltiptext">pokemon</span></div>
+        <div className="text-xl p-2 border-b-2 text-center tooltip">Type1<span className="tooltiptext">Type1</span></div>
+        <div className="text-xl p-2 border-b-2 text-center tooltip">Type2<span className="tooltiptext">Type2</span></div>
+        <div className="text-xl p-2 border-b-2 text-center tooltip">Generation<span className="tooltiptext">Generation</span></div>
+        <div className="text-xl p-2 border-b-2 text-center tooltip">Attack<span className="tooltiptext">Attack</span></div>
+        <div className="text-xl p-2 border-b-2 text-center tooltip">Speed<span className="tooltiptext">Speed</span></div>
+        <div className="text-xl p-2 border-b-2 text-center tooltip">Defense<span className="tooltiptext">Defense</span></div>
       </div>
 
       {championsData.map((champion, index) => (
@@ -38,5 +40,3 @@ function LeagueClassic() {
     </div>
   );
 }
-
-export default LeagueClassic;
