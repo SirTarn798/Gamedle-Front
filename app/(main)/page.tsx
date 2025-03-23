b64a4b310b502f188de83301f48ee826347573f0
peth02 <oopeth139oo@gmail.com>
2025-03-23 17:51:59 +0700

    add pokemon main page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <div className="flex justify-center items-center bg-[url(/PokemonMain.png)] hover:bg-[url(/gif/PokemonMain.gif)] bg-cover h-dvh w-1/2 relative group transition-all duration-300">
         {/* Dark overlay that appears on hover */}
         <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-40 transition-opacity duration-300"></div>
-        <a href="/pokemon/classic" className="text-2xl text-shadow-nm relative transform transition-all duration-300 group-hover:scale-125 z-10 ">
+        <a href="/pokemon" className="text-2xl text-shadow-nm relative transform transition-all duration-300 group-hover:scale-125 z-10 ">
           Pokemon
         </a>
       </div>
