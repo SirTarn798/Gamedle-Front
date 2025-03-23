ab4d14897c87b09ecaa87da0704217ec93b93f46
TarnJirayu <jaoaurai@gmail.com>
2025-03-23 16:43:10 +0700

    C:/Program Files/Git/league router page

---
+++ app/(league)/league/page.tsx
@@ -0,0 +1,14 @@
+function League() {
+
+    return (
+        <div className="flex flex-col items-center gap-8 mt-12 w-5/12 max-w-5xl">
+            <h1 className="text-white text-5xl tracking-wider pixelBorder bg-mainTheme">Choose Your Game</h1>
+            <a href="/league/classic" className="text-white text-5xl tracking-wider pixelBorder bg-mainTheme w-full text-center transition-all duration-200 hover:bg-opacity-80 hover:scale-105">Classic</a>
+            <a href="/league/picture" className="text-white text-5xl tracking-wider pixelBorder bg-mainTheme w-full text-center transition-all duration-200 hover:bg-opacity-80 hover:scale-105">Picture</a>
+            <a href="/league/runeterra_reflex" className="text-white text-5xl tracking-wider pixelBorder bg-mainTheme w-full text-center transition-all duration-200 hover:bg-opacity-80 hover:scale-105">Runeterra Reflex</a>
+
+        </div>
+    );
+}
+
+export default League;
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="flex justify-center items-center bg-[url(/LeagueMain.png)] hover:bg-[url(/gif/LeagueMain.gif)] bg-cover h-dvh w-1/2 relative group transition-all duration-300">
         {/* Dark overlay that appears on hover */}
         <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-40 transition-opacity duration-300"></div>
-        <a href="/league/classic" className="text-2xl text-shadow-nm relative transform transition-all duration-300 group-hover:scale-125 z-10">
+        <a href="/league" className="text-2xl text-shadow-nm relative transform transition-all duration-300 group-hover:scale-125 z-10">
           League of Legends
         </a>
       </div>
